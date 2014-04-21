8d03e7df563469777ab9280d0b0928ad513f70a0
fefo <fernando@fernandoaguirre.com.br>
2014-04-21 18:08:41 -0300

    Vr 0.1

---
+++ wp/wp-content/themes/revisao2014/single.php
@@ -0,0 +1,194 @@
+<html>
+  <head>
+    <title>Revisão para quê? | Revisão de texto com bom humor</title>
+    <link href="css/reset.css" type="text/css" rel="stylesheet">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <link href="css/style_internas.css" type="text/css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,400italic,300italic' rel='stylesheet' type='text/css'>
+  	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400italic,700italic' rel='stylesheet' type='text/css'>
+    <meta name="description" content="O Revisão para quê? é um site sobre revisão, redação e leitura de textos.">
+    <meta charset="utf-8">
+  </head>
+<body class="single">
+<!-- ################# cabeçalho ############################ -->
+	<header>
+		<div class="container">
+			<div class="logo"><a href="home.html"><img src="img/logo-revisao-para-que.png" alt="Logotipo Revisão para quê?"></a></div>
+			<div class="info-topo">
+				<nav class="menu-paginas">
+  			 		<ul>
+    			 		<li><a href="">sobre</a></li>
+    			 		<li><a href="">e-book</a></li>
+    			 		<li><a href="">para empresas</a></li>
+    			 		<li><a href="">contato</a></li>
+  			 		</ul>
+  				</nav>
+  				<ul class="social-icons">
+     				<li><a href=""><img src="img/twitter.png" alt="twitter"></a></li>
+     				<li><a href=""><img src="img/facebook.png" alt="facebook"></a></li>
+     				<li><a href=""><img src="img/youtube.png" alt="youtube"></a></li>
+   				</ul>
+  			</div>
+  		</div>
+		<div class="menu-categorias">
+			<div class="container">
+				<nav>
+  					<ul>
+				        <li><a href="">Vida de Revisor</a></li>
+				        <li><a href="">Gramática</a></li>
+				        <li><a href="">Dicas</a></li>
+				        <li><a href="">Redação</a></li>
+				        <li><a href="">Nova ortografia</a></li>
+				        <li><a href="">TCC</a></li>
+   					</ul>
+ 				</nav>
+ 			</div>
+  		</div>
+	</header>
+
+
+
+<!-- ################# conteúdo ########################## -->
+<div class="clearfix container">
+	<div class="content-container">
+		<article>
+			<div class="img-container"><img src="img/img-article.fw.png" alt="Titulo do post"/></div>
+			<h1>TEXTO: 10 PASSOS PARA ESCREVER DE FORMA MAIS EFICIENTE</h1>
+			<div class="post-info">
+				<a class="tag-categoria" href="">vida de revisor</a>
+				<span class="post-data">3 de maio</span>
+			</div>
+			<p class="post-lead">Ut sit amet mi nec nibh rutrum vehicula. Sed ultrices imperdiet mauris a ornare. Aliquam quis ante placerat, fermentum ante nec, tempor nisi.Nibh rutrum vehicula. Sed ultrices imperdiet mauris a ornare.</p>
+
+			<div class="share-buttons">
+				<h4 class="share">Compartilhe</h4 >
+				<span class="fio"></span>
+				<img src="img/img-share.png" class="img-share"/>
+			</div>
+
+			<div class="main-content">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis enim nec ipsum euismod, nec condimentum mi hendrerit. Duis in dolor sed massa rutrum eleifend quis at est. Phasellus odio nisi, consequat ut arcu eu, tempor vehicula sapien. Vestibulum molestie interdum tortor eu accumsan. Donec vitae turpis malesuada, congue nibh euismod, imperdiet tortor. Vivamus eu dapibus nunc. Integer gravida nisi non purus imperdiet ultricies.
+
+				Ut sit amet mi nec nibh rutrum vehicula. Sed ultrices imperdiet mauris a ornare. Aliquam quis ante placerat, fermentum ante nec, tempor nisi. Morbi iaculis sem eget metus scelerisque, quis faucibus odio varius. Vivamus pulvinar sagittis erat id euismod. Vestibulum cursus sed velit vitae hendrerit. Donec tempus mattis turpis, at hendrerit nulla suscipit non.
+
+				Nulla eget diam luctus nunc laoreet feugiat ut fringilla nisl. Integer ut sem tincidunt, feugiat urna non, fringilla nulla. Mauris tincidunt dui quam, gravida vehicula risus rutrum in. Aliquam mollis lobortis ligula, in lobortis massa interdum et. Nam vel tortor a diam tempus euismod a eget tellus. Pellentesque eget tortor venenatis ipsum dapibus molestie pulvinar ut ipsum. Aenean iaculis ut lacus eu dictum. Mauris tempor tristique enim, quis faucibus arcu scelerisque sit amet. Sed rhoncus lectus id gravida interdum. Aenean sed lorem ultrices, blandit metus sed, pellentesque sapien. Mauris ultrices, tellus eu feugiat commodo, tellus augue rutrum justo, eu ultrices elit nulla et arcu. Maecenas volutpat, urna adipiscing feugiat dignissim, ipsum neque blandit nisi, id dignissim massa nibh dignissim ipsum. Donec vehicula fermentum tincidunt.
+
+				In sit amet eleifend metus, in luctus dolor. Integer dui metus, accumsan vitae fermentum in, lobortis
+
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis enim nec ipsum euismod, nec condimentum mi hendrerit. Duis in dolor sed massa rutrum eleifend quis at est. Phasellus odio nisi, consequat ut arcu eu, tempor vehicula sapien. Vestibulum molestie interdum tortor eu accumsan. Donec vitae turpis malesuada, congue nibh euismod, imperdiet tortor. Vivamus eu dapibus nunc. Integer gravida nisi non purus imperdiet ultricies.
+
+				Ut sit amet mi nec nibh rutrum vehicula. Sed ultrices imperdiet mauris a ornare. Aliquam quis ante placerat, fermentum ante nec, tempor nisi. Morbi iaculis sem eget metus scelerisque, quis faucibus odio varius. Vivamus pulvinar sagittis erat id euismod. Vestibulum cursus sed velit vitae hendrerit. Donec tempus mattis turpis, at hendrerit nulla suscipit non.
+
+				Nulla eget diam luctus nunc laoreet feugiat ut fringilla nisl. Integer ut sem tincidunt, feugiat urna non, fringilla nulla. Mauris tincidunt dui quam, gravida vehicula risus rutrum in. Aliquam mollis lobortis ligula, in lobortis massa interdum et. Nam vel tortor a diam tempus euismod a eget tellus. Pellentesque eget tortor venenatis ipsum dapibus molestie pulvinar ut ipsum. Aenean iaculis ut lacus eu dictum. Mauris tempor tristique enim, quis faucibus arcu scelerisque sit amet. Sed rhoncus lectus id gravida interdum. Aenean sed lorem ultrices, blandit metus sed, pellentesque sapien. Mauris ultrices, tellus eu feugiat commodo, tellus augue rutrum justo, eu ultrices elit nulla et arcu. Maecenas volutpat, urna adipiscing feugiat dignissim, ipsum neque blandit nisi, id dignissim massa nibh dignissim ipsum. Donec vehicula fermentum tincidunt.
+
+				In sit amet eleifend metus, in luctus dolor. Integer dui metus, accumsan vitae fermentum in, lobortis
+			</div>
+		</article>
+
+	<!-- ################# Share ######################### -->
+
+		<section class="share-read-comment">
+			<h4>COMPARTILHE, LEIA MAIS  e COMENTE</h4>
+			<img src="img/img-share.png" class="img-share"/>
+		</section>
+
+
+	<!-- ################# leia mais ######################### -->
+		<section class="leia-mais">
+			<div class="post-box">
+				<a href="">
+		  			<h3>[App] Guia prático da nova ortografia</h3>				
+					<p>Hoje foi ao ar a conversa que tive com o Luciano Larrossa da Escola Freelancer. Lá falamos sobre como dar os rimeiros passos para torna-se um revisor freelancer, entre outros assuntos pertinentes à profissão.</p>
+				</a>
+			</div>
+			<div class="post-box">
+				<a href="">
+		  			<h3>[App] Guia prático da nova ortografia</h3>				
+					<p>Hoje foi ao ar a conversa que tive com o Luciano Larrossa da Escola Freelancer. Lá falamos sobre como dar os rimeiros passos para torna-se um revisor freelancer, entre outros assuntos pertinentes à profissão.</p>
+				</a>
+			</div>
+			<div class="post-box">
+				<a href="">
+		  			<h3>[App] Guia prático da nova ortografia</h3>				
+					<p>Hoje foi ao ar a conversa que tive com o Luciano Larrossa da Escola Freelancer. Lá falamos sobre como dar os rimeiros passos para torna-se um revisor freelancer, entre outros assuntos pertinentes à profissão.</p>
+				</a>
+			</div>
+		 </section>
+
+		 <!-- ################# Comments ######################## -->
+		 <section class="comments">
+		 	<p>Comentários aqui =)</p>
+		 </section>
+	</div><!-- End Content container -->
+
+
+	<!-- ################# Sidebar ######################## -->
+	<aside class="sidebar">
+
+		<!-- Newsletter -->
+		<div class="newsletter">
+			<form action="">
+				<h3>Receba novidades esclusivas no seu e-mail!</h3>
+				<input type="text" placeholder="Qual é o seu e-mail?">
+				<button class="btn">cadastrar</button>		
+			</form>
+		</div>
+
+		<!-- Author Info -->
+		<div class="author-info">
+			<div class="author-photo"><img src="img/author-photo.png"></div>
+			<h4 class="">Carolina Machado</h4>
+			<p>Idealizou este blog e, desde então, briga diariamente com HTML, CSS e design. Graduada em Letras pela PUCRS. Revisora de textos e editora de conteúdo. Tem pretensões de dominar o mundo, só não sabe muito bem como.<p>
+			<a href="">Conheça o autor.</a>
+			<a href="">Outros posts deste autor.</a>
+		</div>
+
+	</aside>
+</div><!-- End Container -->
+
+
+<!-- ################# rodapé ############################ -->
+<footer>
+	<div class="container">
+		<aside>
+			<div>
+				<a href="#um">
+					<h3>Solicite uma revisão</h3>
+					<p>Saiba mais sobre o nosso método de trabalho e como entrar em contato conosco.</p>
+					<a href="#dois" class="tag-categoria">acessar</a>
+					</a>
+				</div>
+		</aside>
+		<aside>
+			<div>
+				<a href="">
+					<h3>Calculadora de laudas</h3>
+					<p>Nunca foi tão fácil calcular laudas. Experimente. =)</p>
+					<a href="" class="tag-categoria">acessar</a>
+					</a>
+				</div>
+		</aside>
+		<aside>
+			<div>
+				<a href="">
+					<h3>Calculadora de laudas</h3>
+					<p>Nunca foi tão fácil calcular laudas. Experimente. =)</p>
+					<a href="" class="tag-categoria">acessar</a>
+					</a>
+				</div>
+		</aside>
+	</div>
+		<section class="creative-commons">
+			<div class="container">
+				<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="img/copy.png"></a>
+  				<p>2011-2014 revisaoparaque.com - Alguns direitos reservados. <a href="">Conheça.</a></p>
+  				<a class="logo-fefo"><img src="img/logo-fernando-aguirre.png" alt="logo-fefo"></a>
+				</div>
+		</section>
+	</div>
+</footer>
+
+
+</body>
+</html>
