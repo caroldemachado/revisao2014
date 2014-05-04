e92ca3a859a18bf280bb0df6d61a7538acbd3459
fefo <fernando@fernandoaguirre.com.br>
2014-05-04 18:50:49 -0300

    0.13
    Add query post in index.

--- wp/wp-content/themes/revisao2014/index.php
+++ wp/wp-content/themes/revisao2014/index.php
@@ -53,7 +53,11 @@
 		<section class="conteudo-geral">
 			<div class="container clearfix">
 				<!--  Repete aqui-->
-				 <?php query_posts($query_string.'showposts=12'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				 <?php // query_posts($query_string.'showposts=12'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				
+				<?php
+				query_posts( array( 'posts_per_page' => 12, 'offset' => 4) );
+				if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 				<article class="post-box">
 					<a href="<?php the_permalink() ?>">
